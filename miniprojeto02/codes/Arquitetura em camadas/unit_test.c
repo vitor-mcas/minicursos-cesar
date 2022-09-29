af51391a519e1b109b13793f1fc5e065a2bf2a84
Vitoriano Medeiros Casas <vitorianom37@gmail.com>
2022-09-29 09:16:24 -0400

    Change UT file

--- miniprojeto02/codes/Arquitetura em camadas/unit_test.c
+++ miniprojeto02/codes/Arquitetura em camadas/unit_test.c
@@ -1,16 +1,20 @@
 #include <assert.h>
 #include "ui.h"
 
-// In my_sum.c
+// In my_sum.c exemple
 int my_sum(int a, int b) {
   return a + b;
 }
 
-// In test_my_sum.c
 int main(int argc, char *argv[]) {
   assert(1 == updateMenu(MENU1));
   assert(12 == updateMenu(MENU2));
   assert(13 == updateMenu(MENU3));
+
+  /* Test my_sum */
+  assert(2 == my_sum(1, 1));
+  assert(-2 == my_sum(-1, -1));
+  assert(0 == my_sum(0, 0));
   // ...
   return(0);
 }
